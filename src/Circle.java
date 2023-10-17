5900c4014baebdda46634dd5fd93c7557eef190a
yegrik <134066753+yegrik@users.noreply.github.com>
2023-10-18 01:36:29 +0300

    Update Circle.java

--- src/Circle.java
+++ src/Circle.java
@@ -1,5 +1,6 @@
 public class Circle {
     public Point centre;
+    public double r;
 
     public void setCenterX(double x){
         this.centre.x = x;
@@ -9,6 +10,10 @@ public class Circle {
         this.centre.y = y;
     }
 
+    public void setRad(double r){
+        this.r = r;
+    }
+
     public double getCentreX() {
         return centre.x;
     }
@@ -16,9 +21,13 @@ public class Circle {
     public double getCentreY() {
         return centre.y;
     }
+    
+    public double getRad() {
+        return r;
+    }
 
     @Override
     public String toString(){
-        return String.format("x = %f\ny = %f", centre.x, centre.y);
+        return String.format("x = %f\ny = %f\nr = %f", centre.x, centre.y, r);
     }
 }
