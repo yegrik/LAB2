62935134381211b68a4c159b7d7ddebc84002dfc
yegrik <134066753+yegrik@users.noreply.github.com>
2023-10-18 01:56:47 +0300

    Update DogHouse.java

--- src/DogHouse.java
+++ src/DogHouse.java
@@ -12,7 +12,11 @@ public class DogHouse {
 
     public static void main(String[] args) {
         Dog waw = new Dog("Shura", 3);
+        Dog waw1 = new Dog("Pip", 7);
+        Dog waw2 = new Dog("Pop", 9);
         addDog(waw);
+        addDog(waw1);
+        addDog(waw2);
         System.out.println(dogs);
     }
 
